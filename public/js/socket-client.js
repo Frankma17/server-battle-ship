155dd3874fa7de73ccbf103c770daef21b562f8a
Frankma0117 <franmacagu@gmail.com>
2023-03-31 22:12:03 -0500

    Version inicial del server , validación de conexiones y host del juego

---
+++ public/js/socket-client.js
@@ -0,0 +1,62 @@
+const lblOnline = document.querySelector("#lblOnline");
+const lblOffline = document.querySelector("#lblOffline");
+const txtMensaje = document.querySelector("#txtMensaje");
+const btnEnviar = document.querySelector("#btnEnviar");
+const customerList = document.querySelector("#customer-list");
+
+const socket = io();
+
+//Escuchar algun evento
+socket.on("connect", () => {
+  lblOffline.style.display = "none";
+  lblOnline.style.display = "";
+});
+
+socket.on("disconnect", () => {
+  lblOffline.style.display = "";
+  lblOnline.style.display = "none";
+  console.log("Estas desconectado");
+});
+
+socket.on("enviar-mensaje", (payload) => {
+  console.log(payload);
+});
+
+socket.on("send-host", (payload) => {
+    if(payload!=null){  console.log(`El host es ${payload.name}`);}
+
+});
+
+socket.on("send-customers", (payload) => {
+  if (payload.length != 0) {
+    while (customerList.firstChild) {
+      customerList.removeChild(customerList.firstChild);
+    }
+    const li = document.createElement("li");
+    li.classList.add("list-group-item");
+    li.classList.add("active");
+    li.textContent = "Listado de usuarios";
+    customerList.appendChild(li);
+
+    payload.forEach((customer) => {
+      const li = document.createElement("li");
+      li.classList.add("list-group-item");
+      li.textContent = `${customer.name}`;
+      customerList.appendChild(li);
+    });
+    console.log(payload);
+  }
+});
+
+btnEnviar.addEventListener("click", () => {
+  const name = txtMensaje.value;
+  const payload = {
+    id: socket.id,
+    name: name,
+  };
+  console.log(name);
+
+  socket.emit("customer-register", payload, (id) => {
+    console.log(id);
+  });
+});
---
+++ sockets/controller.js
@@ -0,0 +1,58 @@
+
+let totalCustomers = 0;
+const customers = [];
+let hostCustomer = customers[0];
+const socketController=(socket,io) =>{
+
+    console.log('Cliente conectado', socket.id)
+    totalCustomers +=1;
+
+    socket.on('disconnect', ()=>{
+        console.log('Cliente desconectado' , socket.id );
+        deleteById(socket.id);
+        console.log('El usuario se eliminó con éxito');
+        console.log('La lista de usuarios es : ')
+        console.log(customers);
+        io.emit('send-customers',customers);
+        hostCustomer = customers[0] 
+        io.emit('send-host',hostCustomer);
+    });
+
+    socket.on('customer-register',(payload,callback)=>{
+        const customer = {
+            id : payload.id,
+            name:  payload.name
+        }
+        console.log('El usuario se agregó con éxito');
+        console.log(customer);
+        customers.push(customer);
+        console.log('La lista de usuarios es : ')
+        console.log(customers);
+        io.emit('send-customers',customers);
+        hostCustomer = customers[0]
+        io.emit('send-host',hostCustomer);
+    });
+
+
+
+    socket.on('enviar-mensaje',(payload, callback)=>{
+        console.log('Recibir el mensaje del client')
+        console.log(payload);
+        const id = 1214
+        callback(id)
+        //Emitir mensaje a todos menos al cliente que lo lanza
+        socket.broadcast.emit('enviar-mensaje',payload);
+    });
+
+}
+
+function deleteById(id) {
+    const index = customers.findIndex(customer => customer.id === id);
+    if (index !== -1) {
+        customers.splice(index, 1);
+    }
+}
+
+module.exports = {
+    socketController
+}
